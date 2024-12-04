1750d1db2b7427dab2e3730d6f2f570f54bd37ec
lauglez01 <lauragonzalez33312@gmail.com>
2024-12-04 09:13:20 +0100

    error sin clase

--- client/src/helpers/promiseHelper.js
+++ client/src/helpers/promiseHelper.js
@@ -3,15 +3,12 @@ import { LatestNews } from "../classes/latestNews";
 export const parseNews = (json) => {
   return new Promise((resolve, reject) => {
     try {
-      
       const newsList = json.map(news => {
-        return new LatestNews(news.title, news.source, news.url);
+      return new LatestNews(news.title, news.source, news.url);
       });
       resolve(newsList);
     } catch (error) {
-      error = new Error("object is null or undefined");
-      error.name = "parseNews";
-      reject(error);
+      reject('Error fetching and parsing news:', error);
     }
   });
 };
